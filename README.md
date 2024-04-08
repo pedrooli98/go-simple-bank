fed44a26ecd3beb74ee7807336d529ae899c08b3
Pedro Oliveira <pedrooli98@outlook.com>
2024-04-08 20:47:32 -0300

    feat: document database database documentation and ER model

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This repository contains the code for the **[Backend Master Class [Golang + Post
 ## Project: Simple Bank Service
 
 The course project involves building a backend service for a simple bank application.
+
+<iframe width="560" height="315" src='https://dbdiagram.io/e/661468f103593b6b6181e75d/66146a0c03593b6b6181f44f'> </iframe>
---
+++ docs/db.sql
@@ -0,0 +1,45 @@
+CREATE SCHEMA "simplebank";
+
+CREATE TABLE "simplebank"."accounts" (
+  "id" bigserial PRIMARY KEY,
+  "owner" varchar NOT NULL,
+  "balance" bigint NOT NULL,
+  "currency" varchar NOT NULL,
+  "created_at" timestampz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "simplebank"."entries" (
+  "id" bigserial PRIMARY KEY,
+  "account_id" bigint,
+  "amount" bigint NOT NULL,
+  "created_at" timestampz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "simplebank"."transfers" (
+  "id" bigserial PRIMARY KEY,
+  "from_account_id" bigint,
+  "to_account_id" bigint,
+  "amount" bigint NOT NULL,
+  "name" varchar,
+  "created_at" timestampz NOT NULL DEFAULT (now())
+);
+
+CREATE INDEX ON "simplebank"."accounts" ("owner");
+
+CREATE INDEX ON "simplebank"."entries" ("account_id");
+
+CREATE INDEX ON "simplebank"."transfers" ("from_account_id");
+
+CREATE INDEX ON "simplebank"."transfers" ("to_account_id");
+
+CREATE INDEX ON "simplebank"."transfers" ("from_account_id", "to_account_id");
+
+COMMENT ON COLUMN "simplebank"."entries"."amount" IS 'can be negative or positive';
+
+COMMENT ON COLUMN "simplebank"."transfers"."amount" IS 'can only be positive';
+
+ALTER TABLE "simplebank"."entries" ADD FOREIGN KEY ("account_id") REFERENCES "simplebank"."accounts" ("id");
+
+ALTER TABLE "simplebank"."transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "simplebank"."accounts" ("id");
+
+ALTER TABLE "simplebank"."transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "simplebank"."accounts" ("id");
